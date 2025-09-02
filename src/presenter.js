2580ee56d96197491972cbfc2865b5b3f0595e4e
Camila <camila.carvajal@ucb.edu.bo>
2025-09-02 08:06:51 -0400

    refactor: arreglo lógica clase tennisScorer + fix: recibir valor nulo del index.html

--- src/presenter.js
+++ src/presenter.js
@@ -1,17 +1,19 @@
 import TennisScorer from "./tennisScorer";
 
-const score01 =document.querySelector("jugador1")
-const score02 =document.querySelector("jugador2")
+const score01 =document.querySelector("#jugador1");
+const score02 =document.querySelector("#jugador2");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const scorer = new TennisScorer();
-  for (let i=1;i<score01;i++){
+  const scorePlayer01 =   Number.parseInt(score01.value);
+  const scorePlayer02 =   Number.parseInt(score02.value);
+  for (let i=1;i<scorePlayer01;i++){
     scorer.player1Points++;
   }
-  for (let i=1;i<score02;i++){
+  for (let i=1;i<scorePlayer02;i++){
     scorer.player2Points++;
   }
   div.innerHTML = "<p>" + scorer.showScore() + "</p>";
