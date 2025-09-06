da44d93cd8d2baee80ad19913db83c3db5b186e7
Camila <camila.carvajal@ucb.edu.bo>
2025-09-05 22:21:54 -0400

    fix: duplicado de texto precio en presenter.js (era label estado)

--- src/presenter.js
+++ src/presenter.js
@@ -10,7 +10,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   let cantidad_output = "<p> Cantidad: " + ingresarDato(Number.parseInt(cantidad.value)) + "</p>";
   let precio_output = "<p> Precio: " + ingresarDato(Number.parseInt(precio.value)) + "</p>";
-  let codigo_output = "<p> Precio: " + ingresarCodigoEst(codigo.value) + "</p>";
+  let codigo_output = "<p> Estado: " + ingresarCodigoEst(codigo.value) + "</p>";
   let impuesto_output = "<p> Impuesto por el estado de  " + ingresarCodigoEst(codigo.value) + ": " + calcularImpuestoEstado(codigo.value) + "%</p>";
   let precio_neto = "<p> Precio Neto: " + calcularPrecioNeto(Number.parseInt(cantidad.value), Number.parseInt(precio.value)) + "</p>";
   div.innerHTML = cantidad_output + "<br>" + precio_output + "<br>" + codigo_output + "<br>" + impuesto_output + "<br>" + precio_neto;
