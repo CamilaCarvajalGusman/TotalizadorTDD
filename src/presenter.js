ae07f27b49364c1152fe2dda3271888d04478b11
Camila <camila.carvajal@ucb.edu.bo>
2025-09-05 21:37:10 -0400

    refactor: creación de variables outpunt para luego concatenarlas en div.innerHTML

--- src/presenter.js
+++ src/presenter.js
@@ -9,5 +9,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const cantidad_int = Number.parseInt(cantidad.value);
   const precio_int = Number.parseInt(precio.value);
-  div.innerHTML = "<p> Cantidad: " + ingresarDato(cantidad_int)+ "</p> <br> <p>Precio: "+ ingresarDato(precio_int)+"</p>";
+  let cantidad_output = "<p> Cantidad: " + ingresarDato(cantidad_int)+ "</p>";
+  let precio_output = "<p>Precio: "+ ingresarDato(precio_int)+"</p>";
+  div.innerHTML = cantidad_output + "<br>" + precio_output;
 });
